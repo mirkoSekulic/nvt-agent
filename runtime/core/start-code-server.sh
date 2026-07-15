5251c45318c6e7f0caccc4c437354858d0aab1bb
Mirko Sekulic <23359247+mirkoSekulic@users.noreply.github.com>
2026-07-15 08:44:39 +0200

    fix(runtime): disable code-server telemetry

--- runtime/core/start-code-server.sh
+++ runtime/core/start-code-server.sh
@@ -6,5 +6,7 @@ source "$HOME/.nvt-agent/env"
 code-server \
   --bind-addr "0.0.0.0:${CODE_SERVER_PORT}" \
   --auth none \
+  --disable-telemetry \
+  --disable-update-check \
   "${NVT_WORKSPACE}" \
   >"$HOME/.nvt-agent/code-server.log" 2>&1 &
